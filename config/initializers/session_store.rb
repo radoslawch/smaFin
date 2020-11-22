54dfe148580c3afeaf9b77113876653391f47e19
erdees <rds@rds.prv>
2020-11-22 11:53:52 +0100

    Depend cookie security on environment.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_smaFin_session', secure: true
+if Rails.env.test?
+  Rails.application.config.session_store :cookie_store, key: '_smaFin_session', secure: false
+else
+  Rails.application.config.session_store :cookie_store, key: '_smaFin_session', secure: true
+end
